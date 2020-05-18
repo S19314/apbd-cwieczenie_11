ee9cc4f90a4e70df519163dcc1916fca0965e01f
Vladyslav Domariev <s19314@pjwstk.edu.pl>
2020-05-18 17:10:26 +0200

    Version 0.1

---
+++ Models/DoctorsDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cwieczenie11.Models
+{
+    public class DoctorsDbContext : DbContext 
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+
+        public DoctorsDbContext() 
+        { }
+
+        public DoctorsDbContext(DbContextOptions options)
+        : base (options)
+        {
+        
+        
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) 
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Doctor>((builder) =>
+            {
+                builder.HasKey(e => e.IdDoctor); // Определяет это поле как уникальное
+                builder.Property(e => e.IdDoctor).ValueGeneratedOnAdd(); // Автоматическое генерация нового уникального значения в поле IdDoctor при вставлении нового элемента
+
+
+            }
+            );
+            
+        }
+
+
+    }
+}
